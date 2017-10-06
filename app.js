6853f7e048fdc8ac6e2a55551143c11169655789
Michael Li <mxl3@duke.edu>
2017-10-06 19:15:36 -0400

    changed port

--- app.js
+++ app.js
@@ -737,7 +737,7 @@ app.use(function(err, req, res, next) {
 });
 var server_ip_address = '108.167.175.187';
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 1337);
 
 var server = app.listen(app.get('port'), server_ip_address, function() {
   console.log('Express server listening on port ' + server.address().port);
